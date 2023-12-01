1448475421562857c25ad8c26ac16eb14253a4d9
John Mogensen <jamjaws@gmail.com>
2023-12-01 08:09:05 +0100

    day 1 part 2

--- app/src/main/kotlin/com/jamjaws/adventofcode/xxiii/day/Day01.kt
+++ app/src/main/kotlin/com/jamjaws/adventofcode/xxiii/day/Day01.kt
@@ -1,19 +1,43 @@
 package com.jamjaws.adventofcode.xxiii.day
 
-import com.jamjaws.adventofcode.xxiii.App
 import com.jamjaws.adventofcode.xxiii.readInput
 
 class Day01 {
 
-    fun run(text: List<String>): Int =
+    private val spelledDigits = mapOf(
+        "one" to '1',
+        "two" to '2',
+        "three" to '3',
+        "four" to '4',
+        "five" to '5',
+        "six" to '6',
+        "seven" to '7',
+        "eight" to '8',
+        "nine" to '9',
+    )
+
+    fun part1(text: List<String>): Int =
         text.map { line -> line.filter(Char::isDigit) }
             .map { digits -> "${digits.first()}${digits.last()}" }
             .map(String::toInt)
             .sum()
 
+    fun part2(text: List<String>): Int =
+        text.map(::replaceSpelledDigits).let(::part1)
+
+    private fun replaceSpelledDigits(text: String): String =
+        text.indices.map(text::substring)
+            .map { value ->
+                spelledDigits.entries.find { (spelledDigit, _) -> value.startsWith(spelledDigit) }
+                    ?.value
+                    ?: value.first()
+            }.joinToString("")
+
 }
 
 fun main() {
-    val answer = Day01().run(readInput("Day01"))
-    println(answer)
+    val answer1 = Day01().part1(readInput("Day01"))
+    println(answer1)
+    val answer2 = Day01().part2(readInput("Day01"))
+    println(answer2)
 }
