cf5e0c7b85764d031e98220fbb23a43a2806ae34
John Mogensen <jamjaws@gmail.com>
2023-12-02 09:49:38 +0100

    day 2 part 2

--- app/src/main/kotlin/com/jamjaws/adventofcode/xxiii/day/Day02.kt
+++ app/src/main/kotlin/com/jamjaws/adventofcode/xxiii/day/Day02.kt
@@ -6,6 +6,17 @@ class Day02 {
 
     fun part1(text: List<String>): Int = text.map(::parseRow).let(::getPossibleGames).map(Game::id).sum()
 
+    fun part2(text: List<String>): Int =
+        text.map(::parseRow)
+            .map { game ->
+                listOf(
+                    game.sets.mapNotNull(Set::red).max(),
+                    game.sets.mapNotNull(Set::green).max(),
+                    game.sets.mapNotNull(Set::blue).max(),
+                )
+            }
+            .sumOf { (red, green, blue) -> red * green * blue }
+
     private fun parseRow(line: String): Game {
         val id = line.removePrefix("Game ").split(":").first().toInt()
         val sets = line.split(";").map {
@@ -19,11 +30,13 @@ class Day02 {
     }
 
     private fun getPossibleGames(games: List<Game>): List<Game> = games.filter { game ->
-        game.sets.all { set -> set.red <= MAX_RED && set.green <= MAX_GREEN && set.blue <= MAX_BLUE }
+        game.sets.all { set ->
+            (set.red ?: 0) <= MAX_RED && (set.green ?: 0) <= MAX_GREEN && (set.blue ?: 0) <= MAX_BLUE
+        }
     }
 
-    private fun String.getNumberOfCubes(color: String): Int =
-        Regex("(\\d+) $color").find(this)?.groupValues?.get(1)?.toInt() ?: 0
+    private fun String.getNumberOfCubes(color: String): Int? =
+        Regex("(\\d+) $color").find(this)?.groupValues?.get(1)?.toInt()
 
     companion object {
         private const val MAX_RED = 12
@@ -38,12 +51,14 @@ data class Game(
 )
 
 data class Set(
-    val red: Int = 0,
-    val green: Int = 0,
-    val blue: Int = 0,
+    val red: Int? = null,
+    val green: Int? = null,
+    val blue: Int? = null,
 )
 
 fun main() {
     val answer1 = Day02().part1(readInput("Day02"))
     println(answer1)
+    val answer2 = Day02().part2(readInput("Day02"))
+    println(answer2)
 }
